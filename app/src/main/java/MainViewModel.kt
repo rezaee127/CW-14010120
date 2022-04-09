41fbc757b3b738d1a58db51bed557324c2db12af
razi <rezaie127@yahoo.com>
2022-04-10 00:23:01 +0430

    edit enable & disable  of true & false button

--- app/src/main/java/MainViewModel.kt
+++ app/src/main/java/MainViewModel.kt
@@ -1,4 +1,3 @@
-
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
@@ -7,10 +6,10 @@ import com.example.j36.R
 class MainViewModel : ViewModel() {
 
     val numberLiveData = MutableLiveData<Int>(0)
-    val scoreLiveData=MutableLiveData(0)
-    val colorTrueLiveData=MutableLiveData(R.color.purple_500)
-    val colorFalseLiveData=MutableLiveData(R.color.purple_500)
-
+    val scoreLiveData = MutableLiveData(0)
+    val colorTrueLiveData = MutableLiveData(R.color.purple_500)
+    val colorFalseLiveData = MutableLiveData(R.color.purple_500)
+    val statusButtonLLiveData = MutableLiveData<Boolean>(true)
     val messageLiveData = Transformations.map(numberLiveData) {
         when {
             it < questionCount / 2 -> "خیلی مونده"
@@ -28,58 +27,60 @@ class MainViewModel : ViewModel() {
     var backEnabledLiveData = MutableLiveData<Boolean>(false)
 
 
-    private fun setQuestion(i:Int) {
-
-            when (i) {
-                0 -> backEnabledLiveData.value = false
-                questionCount - 1 -> nextEnabledLiveData.value = false
-                else -> {
-                    nextEnabledLiveData.value = true
-                    backEnabledLiveData.value = true
-                }
+    private fun setQuestion(i: Int) {
+        when (i) {
+            0 -> backEnabledLiveData.value = false
+            questionCount - 1 -> nextEnabledLiveData.value = false
+            else -> {
+                nextEnabledLiveData.value = true
+                backEnabledLiveData.value = true
             }
-            questionLiveData.value = QuestionRepository.questionList[i].question
+        }
+        statusButtonLLiveData.value=true
+        questionLiveData.value = QuestionRepository.questionList[i].question
     }
 
-    fun click(){
+    fun click() {
         numberLiveData.value?.let {
             setQuestion(it)
         }
-        colorTrueLiveData.value=R.color.purple_500
-        colorFalseLiveData.value=R.color.purple_500
+        colorTrueLiveData.value = R.color.purple_500
+        colorFalseLiveData.value = R.color.purple_500
     }
 
     fun nextClicked() {
-        numberLiveData.value=numberLiveData.value?.plus(1)
+        numberLiveData.value = numberLiveData.value?.plus(1)
         click()
     }
 
     fun backClicked() {
-        numberLiveData.value=numberLiveData.value?.minus(1)
+        numberLiveData.value = numberLiveData.value?.minus(1)
         click()
     }
 
-    fun trueButtonClicked(){
-        numberLiveData.value?.let{
-            if (QuestionRepository.questionList[it].answer){
-                scoreLiveData.value=scoreLiveData.value?.plus(5)
-                colorTrueLiveData.value=R.color.green
-            }else{
-                scoreLiveData.value=scoreLiveData.value?.minus(5)
-                colorTrueLiveData.value=R.color.red
+    fun trueButtonClicked() {
+        numberLiveData.value?.let {
+            if (QuestionRepository.questionList[it].answer) {
+                scoreLiveData.value = scoreLiveData.value?.plus(5)
+                colorTrueLiveData.value = R.color.green
+            } else {
+                scoreLiveData.value = scoreLiveData.value?.minus(5)
+                colorTrueLiveData.value = R.color.red
             }
         }
+        statusButtonLLiveData.value=false
     }
 
-    fun falseButtonClicked(){
-        numberLiveData.value?.let{
-            if (!QuestionRepository.questionList[it].answer){
-                scoreLiveData.value=scoreLiveData.value?.plus(5)
-                colorFalseLiveData.value=R.color.green
-            }else{
-                scoreLiveData.value=scoreLiveData.value?.minus(5)
-                colorFalseLiveData.value=R.color.red
+    fun falseButtonClicked() {
+        numberLiveData.value?.let {
+            if (!QuestionRepository.questionList[it].answer) {
+                scoreLiveData.value = scoreLiveData.value?.plus(5)
+                colorFalseLiveData.value = R.color.green
+            } else {
+                scoreLiveData.value = scoreLiveData.value?.minus(5)
+                colorFalseLiveData.value = R.color.red
             }
         }
+        statusButtonLLiveData.value=false
     }
 }
--- app/src/main/java/com/example/j36/MainActivity.kt
+++ app/src/main/java/com/example/j36/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
-    val vmodel : MainViewModel by viewModels()
+    val vmodel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,51 +23,59 @@ class MainActivity : AppCompatActivity() {
         val buttonNext = findViewById<Button>(R.id.nextButton)
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
         val questionText = findViewById<TextView>(R.id.tvQuestion)
-        val buttonBack=findViewById<Button>(R.id.backButton)
-        val textViewMessage=findViewById<TextView>(R.id.tvMessage)
-        val buttonTrue=findViewById<Button>(R.id.btn_true)
-        val buttonFalse=findViewById<Button>(R.id.btn_false)
-        val textViewScore=findViewById<TextView>(R.id.tv_score)
+        val buttonBack = findViewById<Button>(R.id.backButton)
+        val textViewMessage = findViewById<TextView>(R.id.tvMessage)
+        val buttonTrue = findViewById<Button>(R.id.btn_true)
+        val buttonFalse = findViewById<Button>(R.id.btn_false)
+        val textViewScore = findViewById<TextView>(R.id.tv_score)
 
 
         progressBar.max = vmodel.questionCount
 
-        val messageObserver=Observer<String>{
-            textViewMessage.text=it
+        val messageObserver = Observer<String> {
+            textViewMessage.text = it
         }
 
         val numberObserver = Observer<Int> { number ->
-            textView.text = (number+1).toString()
-            progressBar.progress = number+1
+            textView.text = (number + 1).toString()
+            progressBar.progress = number + 1
         }
 
-        val buttonEnabledObserver = Observer<Boolean>{  enabled ->
+        val buttonEnabledObserver = Observer<Boolean> { enabled ->
             buttonNext.isEnabled = enabled
         }
 
-        val questionObserver = Observer<String>{ question ->
+        val questionObserver = Observer<String> { question ->
             questionText.text = question
         }
 
-        vmodel.messageLiveData.observe(this,messageObserver)
-        vmodel.questionLiveData.observe(this , questionObserver)
-        vmodel.nextEnabledLiveData.observe(this , buttonEnabledObserver)
-        vmodel.numberLiveData.observe(this , numberObserver)
-        vmodel.backEnabledLiveData.observe(this){
-            buttonBack.isEnabled=it
+        vmodel.messageLiveData.observe(this, messageObserver)
+        vmodel.questionLiveData.observe(this, questionObserver)
+        vmodel.nextEnabledLiveData.observe(this, buttonEnabledObserver)
+        vmodel.numberLiveData.observe(this, numberObserver)
+
+        vmodel.backEnabledLiveData.observe(this) {
+            buttonBack.isEnabled = it
         }
 
-        vmodel.colorFalseLiveData.observe(this){
+        vmodel.colorFalseLiveData.observe(this) {
             buttonFalse.setBackgroundColor(ContextCompat.getColor(this, it))
         }
 
-        vmodel.colorTrueLiveData.observe(this){
+        vmodel.colorTrueLiveData.observe(this) {
             buttonTrue.setBackgroundColor(ContextCompat.getColor(this, it))
         }
 
-        vmodel.scoreLiveData.observe(this){
-            textViewScore.text=it.toString()
+        vmodel.statusButtonLLiveData.observe(this) {
+            buttonTrue.isEnabled = it
+            buttonFalse.isEnabled = it
         }
+
+        vmodel.scoreLiveData.observe(this) {
+            textViewScore.text = it.toString()
+        }
+
+
 
         buttonNext.setOnClickListener {
             vmodel.nextClicked()
@@ -76,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         buttonBack.setOnClickListener {
             vmodel.backClicked()
         }
-        
+
         buttonTrue.setOnClickListener {
             vmodel.trueButtonClicked()
         }
