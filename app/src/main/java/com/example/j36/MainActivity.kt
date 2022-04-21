11463b6b964020a1c44cd0b56402b5d20d53b5aa
razi <rezaie127@yahoo.com>
2022-04-21 13:46:28 +0430

    data binding

--- app/src/main/java/com/example/j36/MainActivity.kt
+++ app/src/main/java/com/example/j36/MainActivity.kt
@@ -7,13 +7,20 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import com.example.j36.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val vModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.score= vModel.numberLiveData.value!!
+
         initViews()
     }
 
